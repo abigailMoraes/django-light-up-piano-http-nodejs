717c14e29209814a4e10fabc78385aa11db7dd95
l8k1b <abigail.theresa.moraes@gmail.com>
2023-04-16 14:01:11 -0700

    Fixed creation of server and socket

--- server.mjs
+++ server.mjs
@@ -4,15 +4,22 @@
 //   res.write("Hello World!");
 //   res.end();
 // }).listen(process.env.PORT);
+import { Server } from "socket.io";
+import http from "http";
 
-const io = require("socket.io")(3000);
-
+const server = http.createServer;
+const io = new Server(server);
 io.on("connection", (socket) => {
   console.log(socket.id);
-
   socket.on("pythonToServer", (data) => {
     console.log("data received" + JSON.stringify(data));
     io.emit("serverToReact", data);
     console.log("emitted data");
   });
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });

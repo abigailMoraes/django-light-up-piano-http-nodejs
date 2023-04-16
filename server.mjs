eda7b24d782c8ee6b497c1623af24c35b4c7f1c4
l8k1b <abigail.theresa.moraes@gmail.com>
2023-04-16 13:47:02 -0700

    Altered server.mjs to listen to python calls on port 3000

--- server.mjs
+++ server.mjs
@@ -1,6 +1,18 @@
-import { createServer } from 'http';
+import { createServer } from "http";
 
 createServer((req, res) => {
-  res.write('Hello World!');
+  res.write("Hello World!");
   res.end();
 }).listen(process.env.PORT);
+
+const io = require("socket.io")(3000);
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("pythonToServer", (data) => {
+    console.log("data received" + JSON.stringify(data));
+    io.emit("serverToReact", data);
+    console.log("emitted data");
+  });
+});

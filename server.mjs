f9d3d3c7862f9881da074ee2c665f5615fe7462b
l8k1b <abigail.theresa.moraes@gmail.com>
2023-04-16 14:19:19 -0700

    Socket.IO is designed to work with 'http.Server' instead of Express request handler functions. This passes an http.Server instance instead of an Express app instance.

--- server.mjs
+++ server.mjs
@@ -6,8 +6,10 @@
 // }).listen(process.env.PORT);
 import { Server } from "socket.io";
 import http from "http";
+import { express } from "express";
 
-const server = http.createServer;
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 io.on("connection", (socket) => {
   console.log(socket.id);
